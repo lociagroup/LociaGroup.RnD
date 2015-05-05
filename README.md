5008b8cca118dd6d781f2feacd34430628d5f4a3
lociagroup <hector.lopez@lociagroup.com>
2015-05-05 16:15:10 -0400

    Sketch of framework with initial thoughts of architecture.

--- README.md
+++ README.md
@@ -2,3 +2,40 @@ LociaGroup.RnD
 ==============
 
 Research and development software repository
+
+SCADA Architecture Orverview
+
+
+* Decentralized distributed application across all platforms.
+* Run a "Tag server" on each platform that shares teh tags in a decentralized way. Using clustered network.
+* Certain connections have direct access to a PLC 
+* Mappings of dataflow between platforms is all stored at each computer.
+* Bootstrapping platforms allows each machine to know its configuration. 
+* Drivers for communicating to PLC
+    * Modbus
+    * DNP
+    * Etc.
+* Drivers need to be created for the system. 
+* How will it communicate?
+* drivers should be plug an playable. With an interface to the main system.The IO server sould handle drivers.
+* 
+
+
+Server Roles 
+- IO Server (Handles Drivers)
+- Object Server (Reads data from IO server for HMI)
+    - Compresses real time data
+    - Publishes real time data for HMI viewers.
+- HMI Viewer (Drawn GUI with data fed from Object Server)
+- HMI Developer (interface to draw the gui from known tags)
+
+
+The HMI viewer, can be a webbrowser that shows a generated graphics.
+
+RabbitMQ data accross the object server and the HMIV clients. 
+
+each HMIV can read off the buffer or broadcast on the buffer. 
+
+
+
+
--- scadapi.master.system.cs
+++ scadapi.master.system.cs
@@ -3,10 +3,98 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace scadapi.master
 {
     class system
     {
+        // Bootstrap on each platform
+        
+        // Configuration for the platform 
+    
     }
+    
+    class boostrap 
+    {
+        //load the role of the machine
+        //is it an object server?
+        //what is the IO server information?
+        //is it a historian?
+        //is it the main system?
+        //redundancy mapping and star topology
+    }
+    
+    
+    
+    class deviceio
+    {
+        //drivers for all IO
+        //list of available IO's
+    }
+    
+    class dictionary
+    {
+        //rabbitmq
+        
+        //add tags to the dictionary
+    
+        //wich adds the 
+    
+        
+        class Send
+        {
+            public static void Main()
+            {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare("hello", false, false, false, null);
+        
+                        string message = "Hello World!";
+                        var body = Encoding.UTF8.GetBytes(message);
+        
+                        channel.BasicPublish("", "hello", null, body);
+                        Console.WriteLine(" [x] Sent {0}", message);
+                    }
+                }
+            }
+        }
+    
+        class Receive
+        {
+            public static void Main()
+            {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare("hello", false, false, false, null);
+        
+                        var consumer = new QueueingBasicConsumer(channel);
+                        channel.BasicConsume("hello", true, consumer);
+        
+                        Console.WriteLine(" [*] Waiting for messages." +
+                                                 "To exit press CTRL+C");
+                        while (true)
+                        {
+                            var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+        
+                            var body = ea.Body;
+                            var message = Encoding.UTF8.GetString(body);
+                            Console.WriteLine(" [x] Received {0}", message);
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    
+    
 }
